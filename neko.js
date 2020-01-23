fe6f41b5fa037ed8251038d4c76eb46e5039f610
Fumenoid <fumenoid@localhost.localdomain>
2020-01-23 20:31:00 +0530

    indexfile changed

--- neko.js
+++ neko.js
@@ -74,3 +74,16 @@ function spawncat() {
         cat.style.top = (y + "px");
     }, 20);
 }
+
+function myAccFuncn() {
+    var x = document.getElementById("demoAcc");
+    if (x.className.indexOf("w3-show") == -1) {
+      x.className += " w3-show";
+      x.previousElementSibling.className += " w3-grey";
+    } else { 
+      x.className = x.className.replace(" w3-show", "");
+      x.previousElementSibling.className = 
+      x.previousElementSibling.className.replace(" w3-grey", "");
+    }
+}
+  
