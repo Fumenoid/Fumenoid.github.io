e11de64b7d84faba55d7a8691df4eb278b762fb4
Fumenoid <fumenoid@localhost.localdomain>
2020-01-23 22:57:56 +0530

    contactusupdated

--- neko.js
+++ neko.js
@@ -86,4 +86,16 @@ function myAccFuncn() {
       x.previousElementSibling.className.replace(" w3-grey", "");
     }
 }
+
+function myAccFuncnn() {
+    var x = document.getElementById("demoAcc2");
+    if (x.className.indexOf("w3-show") == -1) {
+      x.className += " w3-show";
+      x.previousElementSibling.className += " w3-grey";
+    } else { 
+      x.className = x.className.replace(" w3-show", "");
+      x.previousElementSibling.className = 
+      x.previousElementSibling.className.replace(" w3-grey", "");
+    }
+}
   
