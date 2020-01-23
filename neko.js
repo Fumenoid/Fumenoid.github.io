07bf65374ebcd9d3715903cfb249cfc5d2dac95e
Fumenoid <fumenoid@localhost.localdomain>
2020-01-24 01:47:27 +0530

    cybersec updated

--- neko.js
+++ neko.js
@@ -98,4 +98,16 @@ function myAccFuncnn() {
       x.previousElementSibling.className.replace(" w3-grey", "");
     }
 }
+
+function cyber() {
+    var x = document.getElementById("csf");
+    if (x.className.indexOf("w3-show") == -1) {
+      x.className += " w3-show";
+      x.previousElementSibling.className += " w3-grey";
+    } else { 
+      x.className = x.className.replace(" w3-show", "");
+      x.previousElementSibling.className = 
+      x.previousElementSibling.className.replace(" w3-grey", "");
+    }
+}
   
